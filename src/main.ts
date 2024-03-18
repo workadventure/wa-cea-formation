183bc5b4995e7f31dbe82ee7fdcd94bd9cab1c7c
Hugo Averty <hugoaverty@gmail.com>
2024-03-18 15:59:37 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,38 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    var inBubble = WA.sound.loadSound("./webrtc-in.mp3");
+    var outBubble = WA.sound.loadSound("./webrtc-out.mp3");
+    var config = {
+        volume : 0.5,
+        loop : false,
+        rate : 1,
+        detune : 1,
+        delay : 0,
+        seek : 0,
+        mute : false
+    }
+    
+    
+    WA.room.area.onEnter("bubble").subscribe(() => {
+        WA.displayBubble();
+        WA.room.hideLayer("step1overlay");
+        WA.room.hideLayer("step1");
+        WA.room.showLayer("step2");
+        inBubble.play(config);
+        outBubble.stop();
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave("bubble").subscribe(() => {
+        WA.removeBubble();
+        inBubble.stop();
+        outBubble.play(config);
+    })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -26,11 +44,5 @@ WA.onInit().then(() => {
 
 }).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
