040655220487cc9875144d1fba46154a9f17ee3b
Hugo Averty <hugoaverty@gmail.com>
2024-04-03 08:32:55 +0200

    Adding steps and fixing bugs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+let popup: any;
+let popupEntrance: any;
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -37,10 +40,23 @@ WA.onInit().then(() => {
         outBubble.play(config);
     });
 
-    WA.room.area.onEnter("chat").subscribe(() => {
+    WA.room.area.onEnter("jitsi").subscribe(() => {
         WA.room.hideLayer("step2");
         WA.room.showLayer("step3");
+        if(popup) return;
+        popup = WA.ui.openPopup("jitsiPopup", "Vous êtes dans une salle de réunion (visio conférence classique) !" as string, [{
+            label: "Fermer",
+            className: "primary",
+            callback: () => {
+                popup?.close();
+                popup = null;
+            }
+        }]);
     })
+    WA.room.area.onLeave("jitsi").subscribe(() => {
+        popup?.close();
+        popup = null;
+    });
 
     WA.room.area.onEnter("interaction").subscribe(() => {
         WA.room.hideLayer("step3");
@@ -51,6 +67,24 @@ WA.onInit().then(() => {
         WA.room.hideLayer("step4");
         WA.room.showLayer("step5");
     })
+
+
+
+    WA.room.area.onEnter("zoneEntrance").subscribe(() => {
+        if(popupEntrance) return;
+        popupEntrance = WA.ui.openPopup("entrancePopup", "Vous n'avez pas compléter toutes les étapes de la formation. Êtes-vous sur de vouloir rentrer ?" as string, [{
+            label: "Rentrer dans le monde CEA",
+            className: "primary",
+            callback: () => {
+                popup?.close();
+                popup = null;
+            }
+        }]);
+    })
+    WA.room.area.onLeave("zoneEntrance").subscribe(() => {
+        popupEntrance?.close();
+        popupEntrance = null;
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
